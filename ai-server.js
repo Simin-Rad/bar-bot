630d0c299bb43342a93d244be0bfc06f72f85643
Simin-Rad <simin.rad@tum.de>
2024-04-08 22:01:56 +0200

    changes

--- ai-server.js
+++ ai-server.js
@@ -88,7 +88,9 @@ async function run_script(order_url) {
             console.log('script executed successfully');
             ai_results.results = stdout
             ai_results.ai_results_is_set = true
-            const payload = ai_results.results;
+            const payload = {
+                text: ai_results.results
+            };
             axios.put(callbacks.run_callback, payload)
                 .then(response => {
                     console.log('PUT request successful:', response.data);
