b202cb8c518413dd30b4e4d7541eb5d99257ed4a
Simin-Rad <simin.rad@tum.de>
2024-04-14 23:21:57 +0200

    edited

--- ai-server.js
+++ ai-server.js
@@ -248,8 +248,8 @@ async function run_script_ai_order_detection(ordertext) {
             console.log('script executed successfully');
             ai_results.results = stdout
             ai_results.ai_results_is_set = true
-            console.log(stdout)
             const payload = JSON.parse(ai_results.results);
+            console.log(payload)
             axios.put(callbacks.run_callback, payload)
                 .then(response => {
                     console.log('PUT request successful:', response.data);
