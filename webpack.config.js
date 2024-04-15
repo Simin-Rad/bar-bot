a2a9ac55fd7f0999cd5f090dd41c3137fb85a6ea
Simin-Rad <simin.rad@tum.de>
2024-04-15 02:00:50 +0200

    edited

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,9 @@ const webpack = require('webpack');
 const env = dotenv.config().parsed;
 console.log('env.redirection_path:', env.redirection_path);
 console.log('env.redirection_path_put:', env.redirection_path_put);
+console.log('env.redirection_path_download:', env.redirection_path_download);
 console.log('env.redirection_path_ai:', env.redirection_path_ai);
+console.log('env.redirection_path_order_detection:', env.redirection_path_order_detection);
 
 // webpack.config.js
 
@@ -23,7 +25,9 @@ const config1 = {
         new webpack.DefinePlugin({
             'env.redirection_path': JSON.stringify(env.redirection_path),
             'env.redirection_path_put': JSON.stringify(env.redirection_path_put),
-            'env.redirection_path_ai': JSON.stringify(env.redirection_path_ai)
+            'env.redirection_path_download': JSON.stringify(env.redirection_path_download),
+            'env.redirection_path_ai': JSON.stringify(env.redirection_path_ai),
+            'env.redirection_path_order_detection': JSON.stringify(env.redirection_path_order_detection)
 
         })
     ]
